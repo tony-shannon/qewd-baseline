8645eb388f401c57f538c00087615d762c04e4ec
tony-shannon <tony.shannon@frectal.com>
2020-04-25 11:52:36 +0100

    add jsdb document www & qewdapps

--- qewd-apps/covid/getGeoJSON/index.js
+++ qewd-apps/covid/getGeoJSON/index.js
@@ -4,16 +4,43 @@ module.exports = function(messageObj, session, send, finished) {
       return finished({error: 'unauthenticated'});
     }
   
-    let doc = this.db.use('geoJ');
+    let geoJSONplaces = this.db.use('geoJ', 'JSONdata', 'places');
   
-    let results = doc.getDocument(true);
+    let places = geoJSONplaces.getDocument(true);
+    geoJSONplaces.forEachChild(function(name, node) {
+      if (name === messageObj.params.id) {
+        console.log("well node#: " + name + "matches the incoming id request of geoJSON" )
+      }
+      console.log(JSON.stringify(node));
+      console.log(typeof name);
+      console.log(typeof messageObj.params.id);
+      console.log("nodeName is" + name);
+    });
+
+    /*
+    geoJSONplaces.forEachLeafNode(function(value, node) {
+      if (value === messageObj.params.id) {
+        console.log("well node#: matches the incoming id request of geoJSON" )
+      }
+      console.log(JSON.stringify(node));
+      console.log(typeof value);
+      console.log(typeof messageObj.params.id);
+      console.log("nodeName_fELN is" + node.name);
+    });
+*/
+
    // let resultsD = [];
+
+   console.log("##########-------- #######");
+   console.log("MsgObj Params: " + JSON.stringify(messageObj.params))
+   console.log("<<<<<<<<<<<------- >>>>>>>") 
    //let docV = doc;
     //console.log("DocValue is " + doc.value)
-    resultsD.push(doc);
+    //resultsD.push(doc);
   
-    finished({summary: results,
+    finished({geoPlaces: places,
               //docValue: resultsD,
-              docB: doc});
+            //  docB: doc
+            });
   };
   
